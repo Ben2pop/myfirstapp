2f28119894d593d9f7f9767810fbb9abf03da0e6
ben2pop <r.bendenoun@gmail.com>
2013-06-04 21:03:23 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Infogrator::Application.config.secret_token = 'b9e4c2d4b0311a9fd30ac8001ead83261635d80a27efe5c4569b7d42621d1ade6560ca0342ecd2f4c5e0ed8b84b586dacd7baf12232371ff7684409ec9dbe919'
